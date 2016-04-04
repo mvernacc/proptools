e8d428ad6fe8d52802c9f0e7eb0d1af9395ae33c
mvernacc <mvernacc@mit.edu>
2016-04-04 10:38:41 -0400

    Add gravity constant to nozzle.

--- nozzle.py
+++ nozzle.py
@@ -8,6 +8,9 @@ proptools
 # Universal gas constant [units: joule kelvin**-1 mole**-1].
 R_univ = 8.314
 
+# Acceleration due to gravity at Earth's surface [units; meter second**-2]
+g = 9.81
+
 def thrust_coef(p_c, p_e, gamma, p_a=None, er=None):
     '''Nozzle thrust coefficient, C_f.
 
