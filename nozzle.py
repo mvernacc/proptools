34dc76340b45d935aa58ef72ac224826a079d867
mvernacc <mvernacc@mit.edu>
2016-04-18 19:09:38 -0400

    Add Mach-area relation to nozzle.py

--- nozzle.py
+++ nozzle.py
@@ -4,6 +4,16 @@ Matt Vernacchia
 proptools
 2016 Apr 3
 '''
+import numpy as np
+
+# Warning logging.
+import logging
+formatter = logging.Formatter('%(levelname)s {%(pathname)s:%(lineno)d}: %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger = logging.getLogger('simple_example')
+logger.addHandler(ch)
+
 import constants
 
 R_univ = constants.R_univ
@@ -97,6 +107,46 @@ def throat_area(m_dot, p_c, T_c, gamma, m_molar):
     return A_t
 
 
+def mach_from_er(er, gamma):
+    '''
+    Find the exit Mach number from the area expansion ratio.
+    
+    Explicit Inversion of Stodola's Area-Mach Equation
+    Source: J. Majdalani and B. A. Maickie
+    http://maji.utsi.edu/publications/pdf/HT02_11.pdf
+
+    Arguments:
+        er: Diverging nozzle area expansion ratio, A_e / A_t [units: none].
+        gamma: Exhaust gas ratio of specific heats [units: none].
+
+    Returns:
+        The exit Mach number [units: none].
+    '''    
+    n = 5 # order of the aproximation
+    X = np.zeros((n,))
+    M = np.zeros((n,))
+
+    e = 1/float(er) # expansion ratio
+    y = gamma # ratio of specific heats
+    B = (y+1)/(y-1)
+    k = np.sqrt( 0.5*(y-1) )
+    u = e**(1/B) / np.sqrt( 1+k**2 )
+    X[0] = (u*k)**(B/(1-B))
+    M[0] = X[0]
+
+    for i in xrange(1,n):
+        lamb = 1/( 2*M[i-1]**(2/B)*(B-2) + M[i-1]**2 *B**2*k**2*u**2 )
+        X[i] = lamb*M[i-1]*B*( M[i-1]**(2/B) - M[i-1]**2*B*k**2*u**2 \
+            + ( M[i-1]**(2+2/B)*k**2*u**2*(B**2-4*B+4) \
+            - M[i-1]**2*B**2*k**2*u**4 + M[i-1]**(4/B)*(2*B-3) \
+            + 2*M[i-1]**(2/B)*u**2*(2-B) )**0.5 )
+        M[i] = M[i-1] + X[i]
+    if abs( np.imag( M[n-1] ) ) > 1e-5:
+        logger.warning('Exit Mach Number has nonzero imaginary part!')
+    Me = float(np.real(M[n-1]))
+    return Me
+
+
 def main():
     # Do sample problem 1-3 from Huzel and Huang.
     # Inputs
@@ -119,6 +169,15 @@ def main():
     print 'c*  = {0:.0f} m/s, should be {1:.0f} m/s'.format(c, c_hh)
     assert(abs(C_f - C_f_hh) < 0.01)
     assert(abs(c - c_hh) < 5)
+
+    # Test mach_from_er against H&H figure 1-12
+    gamma = 1.4
+    er = 4
+    M_hh = 3.0
+    M = mach_from_er(er, gamma)
+    print 'M  = {0:.3f}, should be {1:.1f}'.format(M, M_hh)
+    assert(abs(M - M_hh) < 0.1)
+
 
 
 if __name__ == '__main__':
