cfd878767ac9a79dca8a919be314de7e219088c2
mvernacc <mvernacc@mit.edu>
2017-10-01 20:13:17 -0400

    Add altitude to thrust plot.

--- docs/source/examples/plots/thrust_pa.py
+++ docs/source/examples/plots/thrust_pa.py
@@ -1,7 +1,8 @@
-"""Plot thrust vs chmaber pressure."""
-
+"""Plot thrust vs ambient pressure."""
+from scipy.optimize import fsolve
 import numpy as np
 from matplotlib import pyplot as plt
+import skaero.atmosphere.coesa as atmo
 from proptools import nozzle
 
 p_c = 10e6   # Chamber pressure [units: pascal]
@@ -14,9 +15,30 @@ C_f = nozzle.thrust_coef(p_c, p_e, gamma,    # Thrust coefficient [units: dimens
                          p_a=p_a, er=nozzle.er_from_p(p_c, p_e, gamma))
 F = A_t * p_c * C_f    # Thrust [units: newton]
 
+ax1 = plt.subplot(111)
 plt.plot(p_a * 1e-3, F * 1e-3)
 plt.xlabel('Ambient pressure $p_a$ [kPa]')
 plt.ylabel('Thrust $F$ [kN]')
-plt.title('Thrust vs ambient pressure at $p_c = {:.0f}$ MPa, $p_e = {:.0f}$ kPa'.format(
+plt.suptitle('Thrust vs ambient pressure at $p_c = {:.0f}$ MPa, $p_e = {:.0f}$ kPa'.format(
     p_c *1e-6, p_e * 1e-3))
+
+# Add altitude on second axis
+ylim = plt.ylim()
+ax2 = ax1.twiny()
+new_tick_locations = np.array([100, 75, 50, 25, 1])
+ax2.set_xlim(ax1.get_xlim())
+ax2.set_xticks(new_tick_locations)
+
+
+def tick_function(p):
+    """Map atmospheric pressure [units: kilopascal] to altitude [units: kilometer]."""
+    h_table = np.linspace(84e3, 0)    # altitude [units: meter]
+    p_table = atmo.pressure(h_table)    # atmo pressure [units: pascal]
+    return np.interp(p * 1e3, p_table, h_table) * 1e-3
+
+
+ax2.set_xticklabels(['{:.0f}'.format(h) for h in tick_function(new_tick_locations)])
+ax2.set_xlabel('Altitude [km]')
+ax2.tick_params(axis='y', direction='in', pad=-25)
+plt.subplots_adjust(top=0.8)
 plt.show()
