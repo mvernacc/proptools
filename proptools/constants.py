c1c62347e17991f33519c7aee28cac1a4722a517
mvernacc <mvernacc@mit.edu>
2017-10-08 15:30:29 -0400

    Add elementary charge and mass to constants.

--- proptools/constants.py
+++ proptools/constants.py
@@ -13,3 +13,9 @@ g = 9.81
 
 # Standard temperature [kelvin]
 T_stp = 273.15
+
+# Elementary charge [units: coulomb].
+charge = 1.602176e-19
+
+# unified atomic mass unit equivalent in kilograms.
+amu_kg = 1.660539e-27
